ff13d4d194bbf8a058549d7f2b9eaa146bd35ba4
AaquilAli <aaquilali2050@gmail.com>
2023-09-06 17:24:02 +0530

    Http server

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	//fmt.Println("Hello World\n")
+	godotenv.Load()
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("Port not found\n")
+	}
+	fmt.Println("Port:", portStr)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		//AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handleReadiness)
+	v1Router.Get("/err", handleErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portStr,
+	}
+	err := srv.ListenAndServe()
+	if err == nil {
+		log.Fatal("Error in Listen and server")
+	}
+}
